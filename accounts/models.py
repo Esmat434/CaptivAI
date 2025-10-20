6de11534b37ffd80497572a95af137056a7c9346
alex <alex@gmail.com>
2025-10-20 08:38:42 -0700

    add customuer model in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    class SocialMediaChoice(models.Choices):
+        instagram = 'Instagaram'
+        linkedin = 'LinkedIn'
+        facebook = 'Facebook'
+        x = 'X'
+
+    email = models.EmailField(verbose_name='email')
+    avatar = models.ImageField(blank=True, null=True, verbose_name='avatar')
+    age = models.IntegerField(default=0, verbose_name='age')
+    socialmedia = models.CharField(max_length=100, choices=SocialMediaChoice, default=SocialMediaChoice.instagram, verbose_name='socialmedia')
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name = 'CustomUser'
+        verbose_name_plural = 'CustomUsers'
