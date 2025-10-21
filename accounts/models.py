0d2db1ab5f8120fe16eaab7a3c46599bf43ea6ab
alex <alex@gmail.com>
2025-10-21 04:22:31 -0700

    fix models in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -4,21 +4,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    class SocialMediaChoice(models.TextChoices):
-        INSTAGRAM = 'Instagram', 'Instagram'
-        LINKEDIN = 'LinkedIn', 'LinkedIn'
-        FACEBOOK = 'Facebook', 'Facebook'
-        X = 'X', 'X'
-
     email = models.EmailField(unique=True, verbose_name='email')
     avatar = models.ImageField(blank=True, null=True, verbose_name='avatar')
-    socialmedia = models.CharField(
-        max_length=100, 
-        choices=SocialMediaChoice, 
-        default=SocialMediaChoice.INSTAGRAM, 
-        verbose_name='socialmedia'
-    )
-    birth_date = models.DateField(verbose_name="birth date")
+    birth_date = models.DateField(blank=True, null=True,verbose_name="birth date")
 
     def __str__(self):
         return self.username
