26981f0b251195f1ab6ca84559989df3a0969e30
alex <alex@gmail.com>
2025-10-20 21:42:30 -0700

    add Custom User on accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -4,20 +4,25 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    class SocialMediaChoice(models.Choices):
-        instagram = 'Instagaram'
-        linkedin = 'LinkedIn'
-        facebook = 'Facebook'
-        x = 'X'
+    class SocialMediaChoice(models.TextChoices):
+        INSTAGRAM = 'Instagram', 'Instagram'
+        LINKEDIN = 'LinkedIn', 'LinkedIn'
+        FACEBOOK = 'Facebook', 'Facebook'
+        X = 'X', 'X'
 
-    email = models.EmailField(verbose_name='email')
+    email = models.EmailField(unique=True, verbose_name='email')
     avatar = models.ImageField(blank=True, null=True, verbose_name='avatar')
-    age = models.IntegerField(default=0, verbose_name='age')
-    socialmedia = models.CharField(max_length=100, choices=SocialMediaChoice, default=SocialMediaChoice.instagram, verbose_name='socialmedia')
+    socialmedia = models.CharField(
+        max_length=100, 
+        choices=SocialMediaChoice, 
+        default=SocialMediaChoice.INSTAGRAM, 
+        verbose_name='socialmedia'
+    )
+    birth_date = models.DateField(verbose_name="birth date")
 
     def __str__(self):
         return self.username
     
     class Meta:
-        verbose_name = 'CustomUser'
-        verbose_name_plural = 'CustomUsers'
+        verbose_name = 'Custom User'
+        verbose_name_plural = 'Custom Users'
