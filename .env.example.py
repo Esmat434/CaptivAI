3b65e26b2677d60fcac6babdff2b46ef56fa1256
alex <alex@gmail.com>
2025-10-27 00:35:23 -0700

    fix .env.example

--- .env.example.py
+++ .env.example.py
@@ -12,21 +12,3 @@ MYSQL_DATABASE=your_db_name
 MYSQL_USER=your_db_user
 MYSQL_PASSWORD=your_db_password
 MYSQL_ROOT_PASSWORD=your_db_root_password
-
-# Celery for redis
-CELERY_BROKER_URL=redis://redis:6379/0
-CELERY_RESULT_BACKEND=redis://redis:6379/0
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
-
-# Email Settings
-EMAIL_HOST=smtp.gmail.com
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
-EMAIL_HOST_USER=your-email@gmail.com
-EMAIL_HOST_PASSWORD=your_password_or_app_password #<-- نکته مهم بعدی
-
-# SITE_DOMAIN
-SITE_DOMAIN=http://localhost:8080
